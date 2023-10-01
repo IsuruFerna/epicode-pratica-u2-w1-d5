a8645a0c1247a8ae088769d13e8472ed8dd9ff63
IsuruFerna <isurumadushan.ferna@gmail.com>
2023-10-01 15:15:22 +0200

    there's a better solution than this

--- base_di_partenza/script.js
+++ base_di_partenza/script.js
@@ -43,21 +43,38 @@ for (let i = 0; i < letters.length; i++) {
 let textBolcks = document.getElementsByClassName("text-block");
 textBolcks = Array.from(textBolcks);
 
+// const renderDefault = function () {
+//    for (let i = 0; i < mainArr.length; i++) {
+//       const containerText = document.querySelector(".text-container");
+//       const row = document.createElement("div");
+//       const span = document.createElement("span");
+//       span.classList.add("letter-original");
+//       row.classList.add("text-row");
+//       row.style.top = `${i}em`;
+//       const rowOriginal = document.createElement("p");
+//       const rowRandom = document.createElement("p");
+//       rowOriginal.classList.add("text-block");
+//       rowRandom.classList.add("text-block");
+//       rowOriginal.innerText = mainArr[i].join(",").replace(/\,/g, "");
+//       rowRandom.innerText = mainArr[i].join(",").replace(/\,/g, "");
+//       row.appendChild(rowOriginal);
+//       row.appendChild(rowRandom);
+//       containerText.appendChild(row);
+//    }
+// };
 const renderDefault = function () {
+   // iterate each column
    for (let i = 0; i < mainArr.length; i++) {
-      const containerText = document.querySelector(".text-container");
-      const row = document.createElement("div");
-      row.classList.add("text-row");
-      row.style.top = `${i}em`;
-      const rowOriginal = document.createElement("p");
-      const rowRandom = document.createElement("p");
-      rowOriginal.classList.add("text-block");
-      rowRandom.classList.add("text-block");
-      rowOriginal.innerText = mainArr[i].join(",").replace(/\,/g, "");
-      rowRandom.innerText = mainArr[i].join(",").replace(/\,/g, "");
-      row.appendChild(rowOriginal);
-      row.appendChild(rowRandom);
-      containerText.appendChild(row);
+      // const containerText = document.querySelector(".text-container");
+      const containerAnimation = document.getElementById("container-animation");
+      const rowLetters = document.createElement("p");
+      rowLetters.classList.add("row-letter");
+
+      // iterate row
+      for (let j = 0; j < mainArr[i].length; j++) {
+         rowLetters.innerHTML += `<span class="letter-original">${mainArr[i][j]}</span>`;
+      }
+      containerAnimation.appendChild(rowLetters);
    }
 };
 renderDefault();
@@ -76,39 +93,115 @@ const RandomOneOrZero = function () {
 const len = mainArr.length;
 const textBlocks = document.querySelectorAll(".text-block");
 
+let rawLetters = document.getElementsByClassName("row-letter");
+rawLetters = Array.from(rawLetters);
+
 const loop = setInterval(function () {
    let temArray = [];
-   let temArray1 = [];
-   for (let i = 0; i < textBlocks.length; i++) {
-      // itterating 2d array
-      if ((i % 2 === 0 && len > i / 2) || i === mainArr.length - 1) {
-         temArray1 = mainArr[i / 2];
-         // make a condition that randomply place between tem array or original
-         // randomply remove characters
 
-         temArray = mainArr[i / 2];
-         const random = generateRandomNum(len / 2);
-         for (let j = 0; j < random; j++) {
-            const randomSecondNum = generateRandomNum(len);
+   // column
+   for (let i = 0; i < mainArr.length; i++) {
+      temArray = mainArr[i];
+      const mainArrRowLength = mainArr[i].length;
 
-            if (RandomOneOrZero()) {
-               temArray[randomSecondNum] = "  ";
-            } else {
-               temArray[randomSecondNum] = " M ";
-            }
+      // adding span to the rest of the letters
+      for (let k = 0; k < temArray.length; k++) {
+         if (temArray[k] === "M") {
+            temArray[k] = `<span class="letter-original">M</span>`;
+         } else if (temArray[k] === " ") {
+            temArray[k] = `<span class="letter-original"> </span>`;
          }
       }
 
-      if (i % 2 !== 0) {
-         textBlocks[i].innerText = temArray.join(",").replace(/\,/g, "");
-      }
+      // generating random animation/patter of row of letters
+      const random = generateRandomNum(mainArrRowLength);
+      for (let j = 0; j < random; j++) {
+         const randomSecondNum = generateRandomNum(mainArrRowLength);
 
-      // textBlocks[i].classList.toggle("text-red");
-      textBlocks[i].classList.add("text-red");
+         if (RandomOneOrZero()) {
+            temArray[
+               randomSecondNum
+            ] = `<span class="letter-original letter-dynamics"> </span>`;
+         } else {
+            temArray[
+               randomSecondNum
+            ] = `<span class="letter-original letter-dynamics">M</span>`;
+         }
+      }
+      rawLetters[i].innerHTML = temArray.join(",").replace(/\,/g, "");
    }
 }, 1000);
+// const loop = setInterval(function () {
+//    let temArray = [];
+//    // const words = document.querySelector(".word-text");
+//    // const word = words.querySelectorAll("span");
+//    // word.forEach((element) => {
+//    //    if (element.classList.contains("word")) {
+//    //       element.classList.remove("word");
+//    //    } else {
+//    //       element.classList.add("word");
+//    //    }
+//    // });
+
+//    for (let i = 0; i < textBlocks.length; i++) {
+//       // itterating 2d array
+//       if ((i % 2 === 0 && len > i / 2) || i === mainArr.length - 1) {
+//          // make a condition that randomply place between tem array or original
+//          // randomply remove characters
+
+//          temArray = mainArr[i / 2];
+//          const random = generateRandomNum(len);
+//          for (let j = 0; j < random; j++) {
+//             const randomSecondNum = generateRandomNum(len);
+
+//             if (RandomOneOrZero()) {
+//                temArray[randomSecondNum] = "  ";
+//             } else {
+//                temArray[randomSecondNum] = " M ";
+//             }
+//          }
+//       }
+
+//       if (i % 2 !== 0) {
+//          textBlocks[i].innerText = temArray.join(",").replace(/\,/g, "");
+//       }
+//       // textBlocks[i].classList.toggle("text-red");
+//       textBlocks[i].classList.add("text-red");
+//    }
+// }, 2000);
 
 // Clear the interval after some time
-// setTimeout(function () {
-//    clearInterval(loop);
-// }, 1000);
+setTimeout(function () {
+   clearInterval(loop);
+}, 2000);
+
+// if (RandomOneOrZero()) {
+//    // temArray[randomSecondNum].includes("letter-dynamics"
+//    if (
+//       temArray[randomSecondNum] ===
+//       `<span class="letter-original letter-dynamics"> </span>`
+//    ) {
+//       temArray[randomSecondNum] ===
+//          `<span class="letter-original"> </span>`;
+//    } else if (
+//       temArray[randomSecondNum] ===
+//       `<span class="letter-original"> </span>`
+//    ) {
+//       temArray[randomSecondNum] ===
+//          `<span class="letter-original letter-dynamics"> </span>`;
+//    }
+// } else {
+//    if (
+//       temArray[randomSecondNum] ===
+//       `<span class="letter-original letter-dynamics">M</span>`
+//    ) {
+//       temArray[randomSecondNum] ===
+//          `<span class="letter-original">M</span>`;
+//    } else if (
+//       temArray[randomSecondNum] ===
+//       `<span class="letter-original">M</span>`
+//    ) {
+//       temArray[randomSecondNum] ===
+//          `<span class="letter-original letter-dynamics">M</span>`;
+//    }
+// }
